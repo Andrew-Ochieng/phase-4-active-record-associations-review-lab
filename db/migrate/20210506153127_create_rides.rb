f98b732cc1501c0aac14371d20bfee4c894b9e3e
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-11-28 11:09:37 +0300

    Completed assignment

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.integer :taxi_id
+      t.integer :passanger_id
       t.timestamps
     end
   end
